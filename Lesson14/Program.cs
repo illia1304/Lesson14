6aa8f2e1e86faf6ec606b5a2d0e8bac62329959b
Illia <1304illia@gmail.com>
2023-10-03 13:38:57 +0300

    Products, Buyer

--- Lesson14/Program.cs
+++ Lesson14/Program.cs
@@ -1,14 +1,56 @@
-﻿namespace Lesson14
+﻿using System;
+
+namespace Lesson14
 {
     /*1.Goods
       2. buyers
       3. shop
       4.receipts 
     */
+
+    struct Products
+    {
+        public string name;
+        public float price;
+        public int quantity;
+
+        public Products(string? name, float price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+
+    public struct Buyer
+    {
+        public string name;
+        public int id;
+
+        public Buyer(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+
+    public class Shop
+    {
+        public Shop()
+        {
+
+        }
+    }
+
     class Programme
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            float price = float.Parse(Console.ReadLine());
+            int quantity = int.Parse(Console.ReadLine());
+
+            Products products = new Products(name,price, quantity);
 
         }
     }
