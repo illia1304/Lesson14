da54c51ac17d97ce61b4caebac5850093c9886cc
Illia <1304illia@gmail.com>
2023-10-03 12:52:26 +0300

    Add project files.

---
+++ Lesson14/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson14
+{
+    /*1.Goods
+      2. buyers
+      3. shop
+      4.receipts 
+    */
+    class Programme
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
