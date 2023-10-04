a35d14b9bae3ff2c146288816a1feacd5ee0a0c2
Illia <1304illia@gmail.com>
2023-10-04 11:38:48 +0300

    done

--- Lesson14/Program.cs
+++ Lesson14/Program.cs
@@ -27,23 +27,72 @@ namespace Lesson14
     {
         public string name;
         public int id;
+        public float spentMoney;
 
-        public Buyer(string name, int id)
+        public Buyer(string name, int id, float spentMoney)
         {
             this.name = name;
             this.id = id;
+            this.spentMoney = spentMoney;
         }
     }
 
     public class Shop
     {
+        Products[] products = new Products[10];
+        int countP = 0;
+
+        Buyer[] buyers = new Buyer[10];
+        int countB = 1;
         public Shop()
         {
 
         }
+
+        public void BuyProduct()
+        {
+            Console.WriteLine("What product do you want to buy?");
+            string name = Console.ReadLine();
+
+            for(int i = 0; i < products.Length; i++)
+            {
+                if (products[i].name == name)
+                {
+                    buyers[countP] = new Buyer($"buyer{countB}", countB, products[i].price * products[i].quantity);
+
+                    products[i].quantity--;
+                }
+            }
+            
+        }
+        public void ListOfProducts()
+        {
+            for (int i = 0; i < countP; i++)
+            {
+                Console.WriteLine($"{products[i].name}, {products[i].price}, {products[i].quantity}");
+            }
+        }
+        public void Receips()
+        {
+            for(int i = 1; i <= buyers.Length; i++)
+            {
+                Console.WriteLine($"{buyers[i].name}{i}, {buyers[i].id}, {buyers[i].spentMoney}");
+            }
+        }
+        public void AddProduct(string name, float price, int quantity)
+        {
+            if(products.Length > countP)
+            {
+                products[countP] = new Products(name, price, quantity);
+                countP++;
+
+                Console.WriteLine("Product added");
+            }
+
+        }
     }
 
-    public class ShopInterface
+    public class ShopInterface: Shop
     {
         public ShopInterface()
         {
@@ -63,12 +112,24 @@ namespace Lesson14
             switch(index)
             {
                 case 1:
+                    BuyProduct();
                     break;
                 case 2:
+                    ListOfProducts();
                     break;
                 case 3:
+                    Receips();
                     break;
                 case 4:
+                    Console.WriteLine("Enter name of product:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter price of product:");
+                    float price = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter quantity of product:");
+                    int quantity = int.Parse(Console.ReadLine());
+                    
+                    
+                    AddProduct(name, price, quantity);
                     break;
                 default:
                     Console.WriteLine("Invalid value");
@@ -83,12 +144,13 @@ namespace Lesson14
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            float price = float.Parse(Console.ReadLine());
-            int quantity = int.Parse(Console.ReadLine());
+            Shop shop = new Shop();
+            ShopInterface shopInterface = new ShopInterface();
 
-            Products products = new Products(name,price, quantity);
-
+            while(true)
+            {
+                shopInterface.Inteface();
+            }
         }
     }
 }
