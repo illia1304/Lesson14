61753e6f93d0d46c1157442f19bc85d29345f3ca
Illia <1304illia@gmail.com>
2023-10-16 01:01:16 +0300

    refactoring

--- Lesson14/Program.cs
+++ Lesson14/Program.cs
@@ -5,9 +5,7 @@ using System.Linq;
 namespace Lesson14
 {
     public record Product(string name, float price);
-
     public record Buyer(string name = "Orest");
-
     public class UserInteraction
     {
         public bool ContinueShopping()
@@ -15,40 +13,40 @@ namespace Lesson14
             Console.Write("Do you want to continue shopping? (yes/no)");
 
             string answer = Console.ReadLine().ToLower();
-
-            return answer == "yes";
+            if(answer == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
-
-    public class Shop
+    public class ProductManeger
     {
-        private Receipt receipt = new();
         private Dictionary<Product, int> products = new Dictionary<Product, int>();
-        private Buyer buyer = new();
+        private bool wasBought = false;
 
-        UserInteraction userInteraction = new UserInteraction();
-
-        public Shop()
-        {
-
+        public Product GetProductByName(string productName)
+            {
+            foreach (var product in products.Keys)
+            {
+               if (product.name == productName)
+               {
+                    return product;
+                }
+            }
+            return null;
         }
-
-        public void BuyProduct(string productName, int quantity)
+        public bool BuyProduct(Product product, int quantity)
         {
-            Product product = products.Keys.FirstOrDefault(p => p.name == productName);
             if (products.TryGetValue(product, out int CurrentQuantity))
             {
                 if (CurrentQuantity >= quantity)
                 {
-                    float totalPrice = quantity * product.price;
                     products[product] = CurrentQuantity - quantity;
-
-                    receipt.AddPurchase(product, quantity, buyer.name);
-
-                    if (!userInteraction.ContinueShopping())
-                    {
-                        receipt.PrintReceipt();
-                    }
+                    wasBought = true;
                 }
                 else
                 {
@@ -57,10 +55,10 @@ namespace Lesson14
             }
             else
             {
-                Console.WriteLine($"{productName} not found in shop");
+                Console.WriteLine("We haven't that product in our shop");
             }
+            return wasBought;
         }
-
         public void ListOfProducts()
         {
             foreach (var product in products)
@@ -71,6 +69,11 @@ namespace Lesson14
 
         public void AddProduct(Product product, int quantity)
         {
+            if (quantity < 0)
+            {
+                Console.WriteLine("Count can't be < 0. Stop trying broke my program. Default quantity 10");
+                quantity = 10;
+            }
             if (products.ContainsKey(product))
             {
                 products[product] += quantity;
@@ -81,65 +84,64 @@ namespace Lesson14
             }
             Console.WriteLine($"Added {quantity} {product.name} to the shop.");
         }
-
-        public void UpdateBuyerName(string newName)
-        {
-            buyer = new Buyer(newName);
-        }
     }
-
-    public class Receipt
+    public class RecieptManeger
     {
-        private Purchase purchase = new Purchase();
-        private readonly List<Purchase> purchases = new();
-        private Buyer buyer;
-        public float TotalAmount { get; private set; }
-
-        public void AddPurchase(Product product, int quantity, string buyerName)
-        {
-            purchases.Add(new Purchase(product, quantity));
-            TotalAmount += product.price * quantity;
-            buyer = new Buyer(buyerName);
-        }
-
-        public void PrintReceipt()
+        public List<Purchase> purchases = new List<Purchase>();
+        public Buyer buyer = new();
+        ProductManeger productManeger = new ProductManeger();
+        public float totalAmount = 0;
+        
+        public void PrintReciept()
         {
             Console.WriteLine("***Receipt:***");
             Console.WriteLine($"***Buyer: {buyer.name}***");
-            foreach (var purchase in purchases)
-            {
-                Console.WriteLine($"***Product: {purchase.Product.name}\n Quantity: {purchase.Quantity}, Total Price: {purchase.TotalPrice}");
-            }
-            Console.WriteLine($"***Total Amount: {TotalAmount}***");
-        }
-    }
 
+            totalAmount = 0;
+            foreach (var item in purchases)
+            {
+                Purchase purchase = new(item.Product, item.Quantity);
+                Console.WriteLine($"***Product: {item.Product.name}\n Quantity: {item.Quantity}, Total Price: {purchase.TotalPrice}");
+                totalAmount += item.Product.price * item.Quantity;
+            }
+            Console.WriteLine($"***Total Amount: {totalAmount}***");
+        }
+        public void AddPurchase(Product product, int quantity, string buyerName)
+        {
+            Purchase purchase = new(product, quantity);
+            purchases.Add(purchase);
+            totalAmount += purchase.TotalPrice;
+            buyer = new Buyer(buyerName);
+        }
+
+        public void ClearReciept()
+        {
+            purchases.Clear();
+            totalAmount = 0;
+        }
+
+    }
     public class Purchase
     {
         public Product Product { get; }
         public int Quantity { get; }
-        public float TotalPrice => Product.price * Quantity;
-
         public Purchase(Product product, int quantity)
         {
             Product = product;
             Quantity = quantity;
-
         }
 
-        public Purchase()
-        {
-        }
+        public float TotalPrice => Product.price * Quantity;
     }
-
-    public class ShopInterface : Shop
+    public class ShopInterface
     {
-        private Receipt receipt = new Receipt();
+        private ProductManeger productManeger = new();
+        private RecieptManeger recieptManeger = new();
+        private Buyer buyer = new Buyer();
         private UserInteraction userInteraction = new UserInteraction();
-        Buyer buyer = new Buyer();
         public ShopInterface()
         {
-
+            
         }
         public void Inteface()
         {
@@ -148,7 +150,6 @@ namespace Lesson14
             Console.WriteLine("2. List of products");
             Console.WriteLine("3. Add products");
             Console.WriteLine("4. Buyer name change");
-
             int index = int.Parse(Console.ReadLine());
 
             switch (index)
@@ -158,10 +159,20 @@ namespace Lesson14
                     string name = Console.ReadLine();
                     Console.WriteLine("Enter product's quantity which you want to buy:");
                     int quantity = int.Parse(Console.ReadLine());
-                    BuyProduct(name, quantity);
+                    Product product = productManeger.GetProductByName(name);
+
+                    if (productManeger.BuyProduct(product, quantity))
+                    {
+                        recieptManeger.AddPurchase(product, quantity, buyer.name);
+                    }
+                    if (!userInteraction.ContinueShopping())
+                    {
+                        recieptManeger.PrintReciept();
+                        recieptManeger.ClearReciept();
+                    }
                     break;
                 case 2:
-                    ListOfProducts();
+                    productManeger.ListOfProducts();
                     break;
                 case 3:
                     Console.WriteLine("Enter name of product:");
@@ -170,20 +181,19 @@ namespace Lesson14
                     float price = float.Parse(Console.ReadLine());
                     Console.WriteLine("Enter quantity of product:");
                     int _quantity = int.Parse(Console.ReadLine());
-
-                    AddProduct(new Product(_name, price), _quantity);
+                    productManeger.AddProduct(new Product(_name, price), _quantity);
                     break;
                 case 4:
-                    Console.Write("Enter new buyer's name: ");
-                    string newName = Console.ReadLine();
-                    UpdateBuyerName(newName);
-                    Console.WriteLine($"Buyer's name changed to {buyer.name}");
-                    break;
+                   Console.Write("Enter new buyer's name: ");
+                   string newName = Console.ReadLine();
+                   buyer = new Buyer(newName);
+                   Console.WriteLine($"Buyer's name changed to {buyer.name}");
+                   break;
                 default:
-                    Console.WriteLine("Invalid value");
-                    break;
+                   Console.WriteLine("Invalid value");
+                   break;
             }
-
+            
         }
     }
 
@@ -191,6 +201,8 @@ namespace Lesson14
     {
         static void Main(string[] args)
         {
+            ProductManeger productManeger = new ProductManeger();
+            RecieptManeger recieptManeger = new RecieptManeger();
             ShopInterface shopInterface = new ShopInterface();
 
             while (true)
